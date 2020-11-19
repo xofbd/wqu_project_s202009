30053188509c6545bd8a65bfd9d098771543feac
xofbd <dfox09@gmail.com>
2020-11-19 09:56:01 -0500

    Support local deployment of application

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
+SHELL := /bin/bash
+
 all: clean venv
 
 venv: requirements.txt
-    test -d venv || python -m venv venv
-    source venv/bin/activate && pip install -r requirements.txt
-    touch venv
-    
+	test -d venv || python3 -m venv venv
+	source venv/bin/activate && pip install -r requirements.txt
+	touch venv
+
+deploy: venv
+	source venv/bin/activate && bin/run_app
+
 clean:
-    rm -rf venv
+	rm -rf venv
--- wqu_app/app.py
+++ wqu_app/app.py
@@ -1,12 +1,19 @@
+import os
+
 from flask import Flask, request, render_template
 
-from message import greet
+from message import greet, retrieve_local_ip_adress
 
 app = Flask(__name__)
+DEPLOY = os.getenv('DEPLOY')
+
 
 @app.route('/')
 def main():
-    ip_address = request.headers['X-Forwarded-For']
+    if DEPLOY == 'heroku':
+        ip_address = request.headers['X-Forwarded-For']
+    else:
+        ip_address = retrieve_local_ip_adress()
 
     return render_template('index.html', message=greet(ip_address))
 
